d0d2b116fa99e506fbb1f1087fa47a209202335d
Dadadam999 <Antilamer94@gmail.com>
2020-12-13 02:39:30 +0200

    Restructured Component

---
+++ custom/note_timing.lua
@@ -0,0 +1,9 @@
+collection_proxies =
+{
+	[1] = { [1] = {  } }"#main_menu_proxy",
+	[2] = "#lvl1",
+	[3] = "#lvl2",
+	[4] = "#lvl3",
+	[5] = "#lvl4",
+	[6] = "#lvl5"
+}
---
+++ pck/building_blocks/gameobjects/particles/hit_particle.go
@@ -0,0 +1,30 @@
+components {
+  id: "particlefx"
+  component: "/pck/framework/particles/take_damage.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/pck/framework/scripts/auto_play_delete.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
